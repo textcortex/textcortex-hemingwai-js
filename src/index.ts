b014f10d9ca20b0880f7634d100702cb15ad61d6
abrehamgezahegn <gezahegnabereham@gmail.com>
2021-12-14 17:56:24 +0300

    feat: generate blog method done

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ type BuildProps = {
   character_count: number;
   source_language: string;
   creativity: number;
-  n_gen: number;
+  n_gen?: number;
 };
 
 type GenerateProps = {
@@ -19,9 +19,30 @@ type GenerateProps = {
   n_gen?: number;
 };
 
+type GenerateBlogProps = {
+  blog_title: string;
+  character_count: number;
+  creativity: number;
+  source_language: string;
+  blog_categories: string;
+  n_gen?: number;
+};
+
+type RequestData = {
+  prompt: string;
+  category: string;
+  parameters: string;
+  character_count: number;
+  source_language: string;
+  //  Sets string ; number between 0 and 1. Default is 0.65
+  creativity: number;
+  api_key: string;
+  n_gen: number;
+};
+
 class TextCortex {
   private apiKey: string;
-  readonly request: AxiosInstance = axios.create({
+  private request: AxiosInstance = axios.create({
     baseURL: "https://api.textcortex.com/hemingwai",
     headers: {
       "Content-type": "application/json",
@@ -54,9 +75,9 @@ class TextCortex {
     character_count,
     source_language,
     creativity,
-    n_gen,
-  }: BuildProps) {
-    const jsonRequest = {
+    n_gen = 1,
+  }: BuildProps): RequestData {
+    const reqData = {
       prompt: prompt,
       category: category,
       parameters: parameters,
@@ -67,7 +88,7 @@ class TextCortex {
       api_key: this.apiKey,
       n_gen: n_gen,
     };
-    return jsonRequest;
+    return reqData;
   }
 
   async generate({
@@ -76,7 +97,7 @@ class TextCortex {
     source_language,
     character_count,
     creativity,
-    n_gen = 1,
+    n_gen,
   }: GenerateProps) {
     try {
       const data = this.build({
@@ -100,16 +121,54 @@ class TextCortex {
       console.log("error", error.message);
     }
   }
+
+  async generateBlog(input: GenerateBlogProps) {
+    let parameters = "";
+    if (input.blog_categories) {
+      parameters = `Blog Categories: ${input.blog_categories}`;
+    }
+
+    const data = this.build({
+      character_count: input.character_count,
+      source_language: input.source_language,
+      creativity: input.creativity,
+      n_gen: input.n_gen,
+      prompt: input.blog_title,
+      category: "Blog Body",
+      parameters,
+    });
+
+    try {
+      console.log("data", data);
+      const res = await this.request({
+        method: "POST",
+        url: `/generate_text`,
+        data,
+      });
+      console.log("Res", res.data);
+    } catch (error: any) {
+      this.processError(error as AxiosError);
+      console.log("error", error.message);
+    }
+  }
 }
 
 let hemingwai = new TextCortex(
   "gAAAAABht4j63M8buDUobQjj2YvK-aWIlQFZLXmDYg9Wc1hpfwHXDJ2DGvWvJjXlA1owHzag_cgO7htt7ePU4qrYf7fC5dMdO2JQqvxE9I1DBTM_NpY99e9TyWTz5u1oOjtoS24FZaWF"
 );
 
-hemingwai.generate({
-  prompt: "Was ist los mit dir?",
-  parameters: "",
+// hemingwai.generate({
+//   prompt: "Was ist los mit dir?",
+//   parameters: "",
+//   source_language: "de",
+//   character_count: 120,
+//   creativity: 0.7,
+// });
+
+hemingwai.generateBlog({
+  blog_categories: "music",
+  blog_title: "Lights please",
+  character_count: 100,
+  creativity: 0.5,
   source_language: "de",
-  character_count: 120,
-  creativity: 0.7,
 });
