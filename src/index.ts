1e5ed2f17d2a55796b41b21bd15d0a2507a18794
abrehamgezahegn <gezahegnabereham@gmail.com>
2021-12-14 03:21:45 +0300

    feat: project config && generate method done

---
+++ src/index.ts
@@ -0,0 +1,100 @@
+import axios, { AxiosInstance } from "axios";
+
+type BuildProps = {
+  prompt: string;
+  category: string;
+  parameters: string;
+  character_count: number;
+  source_language: string;
+  creativity: number;
+  n_gen: number;
+};
+
+type GenerateProps = {
+  prompt: string;
+  parameters: string;
+  source_language: string;
+  character_count: number;
+  creativity: number;
+  n_gen?: number;
+};
+
+class TextCortex {
+  private apiKey: string;
+  readonly request: AxiosInstance = axios.create({
+    baseURL: "https://api.textcortex.com/hemingwai",
+    headers: {
+      "Content-type": "application/json",
+      Accept: "text/plain",
+    },
+  });
+
+  constructor(apiKey: string) {
+    this.apiKey = apiKey;
+  }
+
+  private build({
+    prompt,
+    category,
+    parameters,
+    character_count,
+    source_language,
+    creativity,
+    n_gen,
+  }: BuildProps) {
+    const jsonRequest = {
+      prompt: prompt,
+      category: category,
+      parameters: parameters,
+      character_count: character_count,
+      source_language: source_language,
+      //  Sets creativity, number between 0 and 1. Default is 0.65
+      creativity: creativity,
+      api_key: this.apiKey,
+      n_gen: n_gen,
+    };
+    return jsonRequest;
+  }
+
+  async generate({
+    prompt,
+    parameters,
+    source_language,
+    character_count,
+    creativity,
+    n_gen = 1,
+  }: GenerateProps) {
+    try {
+      const data = this.build({
+        prompt,
+        category: "Auto Complete",
+        parameters,
+        character_count,
+        source_language,
+        creativity,
+        n_gen,
+      });
+      console.log("data", data);
+      const res = await this.request({
+        method: "POST",
+        url: `/generate_text`,
+        data,
+      });
+      console.log("Res", res.data);
+    } catch (error: any) {
+      console.log("error", error.message);
+    }
+  }
+}
+
+let hemingwai = new TextCortex(
+  "gAAAAABht4j63M8buDUobQjj2YvK-aWIlQFZLXmDYg9Wc1hpfwHXDJ2DGvWvJjXlA1owHzag_cgO7htt7ePU4qrYf7fC5dMdO2JQqvxE9I1DBTM_NpY99e9TyWTz5u1oOjtoS24FZaWF"
+);
+
+hemingwai.generate({
+  prompt: "Was ist los mit dir?",
+  parameters: "",
+  source_language: "de",
+  character_count: 120,
+  creativity: 0.7,
+});
